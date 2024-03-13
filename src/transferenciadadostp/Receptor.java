f1f4f27b36e7d7bb32f35e73c0525ee2d9fa5042
Natan <natanmiguel821@gmail.com>
2024-03-13 10:29:28 -0300

    first commit

---
+++ src/transferenciadadostp/Receptor.java
@@ -0,0 +1,130 @@
+package transferenciadadostp;
+
+import java.util.Arrays;
+import  transferenciadadostp.Transmissor;
+import static transferenciadadostp.Transmissor.dadoBitsCRC;
+import static transferenciadadostp.Transmissor.polimonio;
+import static transferenciadadostp.Transmissor.printBin;
+import static transferenciadadostp.Transmissor.removeZerosEsquerda;
+
+public class Receptor {
+    
+    //mensagem recebida pelo transmissor
+    private String mensagem;
+
+    public Receptor() {
+        //mensagem vazia no inicio da execução
+        this.mensagem = "";
+    }
+    
+    public String getMensagem() {
+        return mensagem;
+    }
+ 
+    private boolean decodificarDado(boolean bits[]){
+        int codigoAscii = 0;
+        int expoente = bits.length-1;
+        
+        //converntendo os "bits" para valor inteiro para então encontrar o valor tabela ASCII
+        for(int i = 0; i < bits.length;i++){
+            if(bits[i]){
+                codigoAscii += Math.pow(2, expoente);
+            }
+            expoente--;
+        }
+        
+        //concatenando cada simbolo na mensagem original
+        this.mensagem += (char)codigoAscii;
+        
+        //esse retorno precisa ser pensado... será que o dado sempre chega sem ruído???
+        return true;
+    }
+    
+    private static boolean decodificarDadoCRC(boolean[] bitsOriginais){
+        
+        //implemente a decodificação Hemming aqui e encontre os 
+        //erros e faça as devidas correções para ter a imagem correta
+        
+        //armazena o dado orignal com os zeros adicionais em um novo vetor
+        for (int bit = 0; bit < bitsOriginais.length; bit++) {
+            if (bit < bitsOriginais.length)
+                bitsOriginais[bit] = bitsOriginais[bit];
+            else
+                bitsOriginais[bit] = false;
+        }
+        System.out.print("Bits com Zeros: ");
+        printBin(bitsOriginais);
+
+        int posicaoInicial = 0;
+        int posicaoFinal = polimonio.length - 1;
+
+        boolean[] resto = new boolean[polimonio.length];
+
+        //bits que serao usados na operacao XOR
+        boolean[] bitsXOR = Arrays.copyOfRange(bitsOriginais, 0, polimonio.length);
+
+        while (posicaoFinal != bitsOriginais.length) {
+            int indiceResto = 0;
+
+            System.out.print("Bits XOR:");
+            printBin(bitsXOR);
+
+            //operacao XOR com o polimonio, registrando o resultado em "resto"
+            for (int indice = 0; indice < polimonio.length; indice++) {
+                resto[indice] = bitsXOR[indice] != polimonio[indice];
+            }
+
+            if (posicaoFinal == bitsOriginais.length - 1)
+                break;
+            
+            //remove os zeros a esquerda do resto e armazena a quantidade removida em zerosRemovidos
+            int zerosRemovidos = removeZerosEsquerda(resto);
+
+            //adiciona bits de volta ao resto a partir de bitsComZeros para compensar os zeros removidos
+            for (int i = 0; i < zerosRemovidos; i++) {
+                System.out.print("Resto: ");
+                printBin(resto);
+                resto[resto.length - zerosRemovidos + i] = bitsOriginais[posicaoFinal];
+                posicaoFinal++;
+            }
+            //atualiza os bits para a proxima operacao, copiando os valores de resto para bitsXOR
+            bitsXOR = Arrays.copyOf(resto, resto.length);
+
+        }
+        
+        System.out.print("Resto Final: ");
+        printBin(resto);
+        //checa se o resto é 0
+        for (boolean bit: resto) {
+            //se houver um bit 1, retorna falso
+            if (bit)
+                return false;
+        }
+        return true;
+        
+    }
+
+    //recebe os dados do transmissor
+    public boolean receberDadoBits(boolean bits[]){
+        
+        //aqui você deve trocar o médodo decofificarDado para decoficarDadoCRC (implemente!!)
+        if (decodificarDadoCRC(bits)) {
+            decodificarDado(bits);
+            return true;
+        }
+
+        //será que sempre teremos sucesso nessa recepção
+        return false;
+    }
+    
+    //teste
+    public static void main(String[] args) {
+        boolean[] mensagem = {true, false, true, true, true};
+        boolean[] mensagemCodificada = dadoBitsCRC(mensagem);
+        
+        if (decodificarDadoCRC(mensagemCodificada))
+            System.out.println("mensagem correta");
+        else
+            System.out.println("mensagem com erro");
+    }
+}
---
+++ src/transferenciadadostp/Transmissor.java
@@ -0,0 +1,163 @@
+package transferenciadadostp;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Transmissor {
+    private String mensagem;
+
+    //polimonio gerador = 1011000110
+    //public static boolean[] polimonio = {true, false, true, true, false, false, false, true, true, false};
+    public static boolean[] polimonio = {true, true, false, true, true, false, false};
+    public Transmissor(String mensagem) {
+        this.mensagem = mensagem;
+    }
+    
+    //convertendo um símbolo para "vetor" de boolean (bits)
+    private boolean[] streamCaracter(char simbolo){
+        
+        //cada símbolo da tabela ASCII é representado com 8 bits
+        boolean bits[] = new boolean[8];
+        
+        //convertendo um char para int (encontramos o valor do mesmo na tabela ASCII)
+        int valorSimbolo = (int) simbolo;
+        int indice = 7;
+        
+        //convertendo cada "bits" do valor da tabela ASCII
+        while(valorSimbolo >= 2){
+            int resto = valorSimbolo % 2;
+            valorSimbolo /= 2;
+            bits[indice] = (resto == 1);
+            indice--;
+        }
+        bits[indice] = (valorSimbolo == 1);
+        
+        return bits;
+    } 
+    
+    //não modifique (seu objetivo é corrigir esse erro gerado no receptor)
+    private void geradorRuido(boolean bits[]){
+        Random geradorAleatorio = new Random();
+        
+        //pode gerar um erro ou não..
+        if(geradorAleatorio.nextInt(5) > 1){
+            int indice = geradorAleatorio.nextInt(8);
+            bits[indice] = !bits[indice];
+        }
+    }
+
+    //remove os zeros a esquerda, retornando a quantidade removida
+    public static int removeZerosEsquerda(boolean[] bits) {
+        int zerosRemovidos = 0;
+        System.out.print("Resto antes de remover zeros: ");
+        printBin(bits);
+        while (!bits[0]) { //enquanto primeiro bit for 0
+            for (int indice = 0; indice < bits.length - 1; indice++) {
+                bits[indice] = bits[indice + 1];
+            }
+            zerosRemovidos++;
+        }
+        return zerosRemovidos;
+    }
+
+    //codifica bits com CRC
+    public static boolean[] dadoBitsCRC(boolean bitsOriginais[]){
+        
+        /*sua implementação aqui!!!
+        modifique o que precisar neste método
+        */
+
+        boolean[] bitsComZeros = new boolean[bitsOriginais.length + (polimonio.length - 1)];
+
+        //armazena o dado orignal com os zeros adicionais em um novo vetor
+        for (int bit = 0; bit < bitsComZeros.length; bit++) {
+            if (bit < bitsOriginais.length)
+                bitsComZeros[bit] = bitsOriginais[bit];
+            else
+                bitsComZeros[bit] = false;
+        }
+
+        int posicaoInicial = 0;
+        int posicaoFinal = polimonio.length - 1;
+
+        boolean[] resto = new boolean[polimonio.length];
+
+        //bits que serao usados na operacao XOR
+        boolean[] bitsXOR = Arrays.copyOfRange(bitsComZeros, 0, polimonio.length);
+
+        while (posicaoFinal != bitsComZeros.length) {
+            int indiceResto = 0;
+
+
+            //operacao XOR com o polimonio, registrando o resultado em "resto"
+            for (int indice = 0; indice < polimonio.length; indice++) {
+                resto[indice] = bitsXOR[indice] != polimonio[indice];
+            }
+
+            if (posicaoFinal == bitsComZeros.length - 1)
+                break;
+            
+            //remove os zeros a esquerda do resto e armazena a quantidade removida em zerosRemovidos
+            int zerosRemovidos = removeZerosEsquerda(resto);
+
+            //adiciona bits de volta ao resto a partir de bitsComZeros para compensar os zeros removidos
+            for (int i = 0; i < zerosRemovidos; i++) {
+                resto[resto.length - zerosRemovidos + i] = bitsComZeros[posicaoFinal];
+                posicaoFinal++;
+            }
+            //atualiza os bits para a proxima operacao, copiando os valores de resto para bitsXOR
+            bitsXOR = Arrays.copyOf(resto, resto.length);
+
+        }
+
+        //copia os bits originais para um novo vetor
+        boolean[] bitsCRC = Arrays.copyOf(bitsOriginais, bitsOriginais.length + (polimonio.length - 1));
+
+        int ultimaPosicaoResto;
+        int ultimaPosicaoBitsCRC;
+
+        //adiciona resto aos bits originais, gerando os bits codificados
+        for (int indice = 0; indice < polimonio.length - 1; indice++) {
+            ultimaPosicaoResto = resto.length - 1 - indice;
+            ultimaPosicaoBitsCRC = bitsCRC.length - 1 - indice;
+            bitsCRC[ultimaPosicaoBitsCRC] = resto[ultimaPosicaoResto];
+
+        }
+        return bitsCRC;
+    }
+    
+    public void enviaDado(Receptor receptor){
+        for(int i = 0; i < this.mensagem.length();i++){
+            boolean bits[] = streamCaracter(this.mensagem.charAt(i));
+
+            //codifica bits
+            boolean bitsCRC[] = dadoBitsCRC(bits);
+            
+            //add ruidos na mensagem a ser enviada para o receptor
+            geradorRuido(bitsCRC);
+            
+            //enviando a mensagem "pela rede" para o receptor (uma forma de testarmos esse método)
+            boolean indicadorCRC = receptor.receberDadoBits(bits);
+            //o que faremos com o indicador quando houver algum erro? qual ação vamos tomar com o retorno do receptor
+        }
+    }
+
+    //teste
+    public static void printBin(boolean[] vetor) {
+        for (boolean a : vetor) {
+            if (a)
+                System.out.print(1);
+            else
+                System.out.print(0);
+        }
+        System.out.println();
+    }
+
+    //teste
+    public static void main(String[] args) {
+        boolean[] mensagem = {true, false, true, true, true};
+        printBin(mensagem);
+        boolean[] mensagemCodificada = dadoBitsCRC(mensagem);
+        printBin(mensagemCodificada);
+    }
+}
